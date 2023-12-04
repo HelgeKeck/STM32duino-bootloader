a720978c27e995046d5a36920aaceafa4812be3b
HelgeKeck <helgekeck@hotmail.com>
2023-12-04 14:06:02 +0100

    x

--- config.h
+++ config.h
@@ -158,7 +158,7 @@
 
     #define LED_BANK            GPIOA
     #define LED_PIN             13
-    #define LED_ON_STATE        1
+    #define LED_ON_STATE        0
 
 // Use Boot1 PB2 as the button, as hardly anyone uses this pin as GPIO
 // Need to set the button input mode to just CR_INPUT and not CR_INPUT_PU_PD because the external pullup on the jumplink is very weak
