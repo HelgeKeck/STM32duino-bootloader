d91c9113362f17a8ee8151e88e4dd84ef6ed3068
HelgeKeck <helgekeck@hotmail.com>
2023-12-19 15:33:56 +0100

    Update hardware.c

--- hardware.c
+++ hardware.c
@@ -154,8 +154,9 @@ void setupCLK(void) {
 
 
 void setupLEDAndButton (void) {
-    SET_REG(AFIO_MAPR,(GET_REG(AFIO_MAPR) & ~AFIO_MAPR_SWJ_CFG) | AFIO_MAPR_SWJ_CFG_NO_JTAG_SW);// Try to disable SWD AND JTAG so we can use those pins (not sure if this works).
-
+    // SET_REG(AFIO_MAPR,(GET_REG(AFIO_MAPR) & ~AFIO_MAPR_SWJ_CFG) | AFIO_MAPR_SWJ_CFG_NO_JTAG_SW);// Try to disable SWD AND JTAG so we can use those pins (not sure if this works).
+    afio_cfg_debug_ports(AFIO_MAPR_SWJ_CFG_FULL_SWJ);
+    afio_cfg_debug_ports(AFIO_MAPR_SWJ_CFG_NO_JTAG_SW);
 #if defined(BUTTON_BANK) && defined (BUTTON_PIN) && defined (BUTTON_PRESSED_STATE)
     SET_REG(GPIO_CR(BUTTON_BANK,BUTTON_PIN),(GET_REG(GPIO_CR(BUTTON_BANK,BUTTON_PIN)) & crMask(BUTTON_PIN)) | BUTTON_INPUT_MODE << CR_SHITF(BUTTON_PIN));
 
